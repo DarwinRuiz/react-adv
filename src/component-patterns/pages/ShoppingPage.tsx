5f355e169302a8dfc69fbacf1ea0bc41ad3cdf7d
Darwin Ruiz <darwinorlandoruiz15@gmail.com>
2024-10-08 17:13:12 -0600

    Updated general styles

--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -4,7 +4,6 @@ import { ProductCard } from '../components/ProductCard'
 import { ProductImage } from '../components/ProductImage'
 import { ProductTitle } from '../components/ProductTitle'
 
-import './../styles/custom-styles.css'
 import { products } from '../data/products'
 
 const product = products[0]
@@ -16,9 +15,7 @@ export const ShoppingPage = (): JSX.Element => {
             <h1>Shopping Store</h1>
             <hr />
 
-            <ProductCard key={product.id} product={product} style={{
-                backgroundColor: '#70D1F8'
-            }}
+            <ProductCard key={product.id} product={product}
 
                 initialValues={{
                     count: 4,
@@ -26,17 +23,11 @@ export const ShoppingPage = (): JSX.Element => {
                 }}
             >
                 {
-                    ({ reset, incrementBy, isMaxCountReached, count }) => (
+                    () => (
                         <>
                             <ProductImage />
                             <ProductTitle />
                             <ProductButton />
-                            <button onClick={reset}>Reset</button>
-                            <button onClick={() => incrementBy(-2)}>-2</button>
-                            {
-                                !isMaxCountReached && <button onClick={() => incrementBy(2)}>+2</button>
-                            }
-                            <span>Count: {count}</span>
                         </>
                     )
                 }
